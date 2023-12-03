bd7dbf5b15488e2c5eaa3e361b2e422ad9810b8d
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-12-03 13:59:02 +0100

    feat: add EasySave program base with a "Hello, world!"

---
+++ EasySave/EasySave/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EasySave
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
