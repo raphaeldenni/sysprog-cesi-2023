96eb98f3a51b6cc1fd1c6cfd4b0b4671130a43e1
atn976 <119575129+atn976@users.noreply.github.com>
2023-12-04 22:07:44 +0100

    feat: listviewmodel
    last save 22:07 04/12/2023

--- EasySave/EasySave/ViewModels/ListViewModel.cs
+++ EasySave/EasySave/ViewModels/ListViewModel.cs
@@ -1,4 +1,4 @@
-﻿using EasySave.Models;
+using EasySave.Models;
 using EasySave.Views;
 using System;
 using System.Collections.Generic;
@@ -13,8 +13,36 @@ namespace EasySave.ViewModels
         public ListView ListView { get; set; }
         public TaskModel TaskModel { get; set; }
 
-        public ListViewModel()
+        public ListViewModel(string[] args)
         {
+            ListView = new ListView();
+            TaskModel = new TaskModel();
+            DisplayTaskList(); // Afficher la liste lors de la création de l'instance
+        }
+
+        public void DisplayTaskList()
+        {
+            List<TaskEntity>? tasks = TaskModel.TasksList;
+
+            if (tasks == null || tasks.Count == 0)
+            {
+                ListView.Message = "Aucune tâche disponible ";
+                ListView.DisplayMessage();
+            }
+            else
+            {
+                foreach (var task in tasks)
+                {
+                    string message = $"ID: {task.Id}, Name: {task.Name}, State: {task.State}, Type: {task.Type}";
+                   ListView.DisplayMessage(message);
+                }
+            }
+        }
+
+        public void UpdateStatFile()
+        {
+            TaskModel.PullStateFile();
+            DisplayTaskList();
         }
     }
 }
