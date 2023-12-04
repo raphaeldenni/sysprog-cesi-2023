67070736d209619a1511acc33e1c13022643e6c7
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-12-05 00:29:20 +0100

    feat: add execution command base

--- EasySave/EasySave/ViewModels/ExecuteViewModel.cs
+++ EasySave/EasySave/ViewModels/ExecuteViewModel.cs
@@ -5,22 +5,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasySave.Types;
 
 namespace EasySave.ViewModels
 {
     public class ExecuteViewModel
     {
         public ExecuteView ExecuteView { get; set; }
-        //public StateM StateModel { get; set; }
+        public HelpView HelpView { get; set; }
+        public TaskModel TaskModel { get; set; }
         public CopyModel CopyModel { get; set; }
         public LogModel LogModel { get; set; }
 
-        public ExecuteViewModel()
+        public ExecuteViewModel(string[] args)
         {
+            ExecuteView = new ExecuteView();
+            HelpView = new HelpView();
+            TaskModel = new TaskModel();
+            
+            if (args.Length != 1)
+            {
+                HelpView.DisplayExecute();
+                HelpView.DisplayMessage();
+            }
+            else
+            {
+                ExecuteTask(args[1]);
+            }
         }
 
-        public void ExecuteTask()
+        public void ExecuteTask(string taskName)
         {
+            var task = TaskModel.TasksList?.FirstOrDefault(t => t.Name == taskName);
+            
+            if (task == null)
+            {
+                ExecuteView.Message = "Task not found";
+                ExecuteView.DisplayMessage();
+            }
+            else
+            {
+                CopyModel = new CopyModel(task.SourcePath, task.DestPath, Enum.Parse<BackupType>(task.Type));
+                CopyModel.CopyFiles();
+                
+                ExecuteView.Message = "Task started";
+                ExecuteView.DisplayMessage();
+            }
         }
     }
 }
