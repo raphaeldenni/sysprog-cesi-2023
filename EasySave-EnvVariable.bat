16dff5049771d52ce7a1e60adf17e509d584dee2
Romain MOREL <119046014+Roooomain@users.noreply.github.com>
2023-12-05 09:49:36 +0100

    Update EasySave-EnvVariable.bat

--- EasySave-EnvVariable.bat
+++ EasySave-EnvVariable.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+
 REM Verifie si le script est execute en tant qu'administrateur
 >nul 2>&1 net session || (
     echo Le script necessite des privileges administratifs. Redemarrez en tant qu'administrateur.
@@ -8,8 +9,10 @@ REM Verifie si le script est execute en tant qu'administrateur
     exit /b 1
 )
 
+
 REM Chemin du repertoire contenant EasySave.exe (demande a l'utilisateur)
 set /p "easySavePath=Entrez le chemin du repertoire contenant EasySave.exe: "
+
 
 REM Verifie si le chemin specifie est un repertoire existant
 if not exist "!easySavePath!\" (
@@ -17,15 +20,19 @@ if not exist "!easySavePath!\" (
     exit /b 1
 )
 
+
 REM Obtient la valeur actuelle de la variable d'environnement PATH
 set "currentPath=!PATH!"
+
 
 REM Verifie si le chemin n'est pas deja present dans la variable PATH
 if not "!currentPath!"=="!currentPath:%easySavePath%=!" (
     echo Le chemin est deja present dans la variable PATH.
 ) else (
+
     REM Ajoute le chemin au debut de la variable PATH
     set "newPath=!easySavePath!;!currentPath!"
+
 
     REM Met a jour la variable d'environnement PATH
     setx PATH "!newPath!" /M
