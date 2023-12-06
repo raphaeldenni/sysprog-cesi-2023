6811b1641acf781bb4674c2171e64b0ab916e1d4
Roooomain <rommor2810@gmail.com>
2023-12-06 16:16:07 +0100

    Create a config files and a config command

---
+++ EasySave/EasySave/Models/ConfigModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace EasySave.Models
+{
+    public class ConfigEntity 
+    {
+        public string LogExtension { get; set; }
+        public string Language { get; set; }
+    }
+
+    public class ConfigModel
+    {
+        private static readonly string ConfigFileName = "config.json";
+        public ConfigEntity Config { get; private set; }
+
+        public ConfigModel()
+        {
+            if (!File.Exists(ConfigFileName))
+            {
+                CreateConfigFile();
+            }
+
+            PullConfigFile();
+        }
+
+        private void CreateConfigFile()
+        {
+            ConfigEntity defaultConfig = new ConfigEntity
+            {
+                LogExtension = ".json",
+                Language = "en"
+            };
+
+            string defaultConfigJson = JsonSerializer.Serialize(defaultConfig);
+            File.WriteAllText(ConfigFileName, defaultConfigJson);
+        }
+
+        private void PullConfigFile()
+        {
+            string configJson = File.ReadAllText(ConfigFileName);
+            Config = JsonSerializer.Deserialize<ConfigEntity>(configJson);
+        }
+
+        public void UpdateConfigFile(string? logExtension, string? lang)
+        {
+            if (logExtension != null)
+            {
+                Config.LogExtension = logExtension;
+            }
+
+            if (lang != null)
+            {
+                Config.Language = lang;
+            }
+
+            string updatedConfigJson = JsonSerializer.Serialize(Config);
+            File.WriteAllText(ConfigFileName, updatedConfigJson);
+        }
+    }
+}
---
+++ EasySave/EasySave/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,61 @@
+﻿using EasySave.Models;
+using EasySave.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace EasySave.ViewModels;
+
+public class ConfigViewModel
+{
+    public ConfigView ConfigView { get; set; }
+
+    public HelpView HelpView { get; set; }
+
+    public ConfigModel ConfigModel { get; set; }
+
+    public ConfigViewModel(string[] args)
+    {
+        ConfigView = new ConfigView();
+        HelpView = new HelpView();
+
+        if (!(args.Length == 2))
+        {
+            HelpView.DisplayCreate();
+            HelpView.DisplayMessage();
+        }
+        else
+        {
+            UpdateConfig(args);
+        }
+    }
+
+    public void UpdateConfig(string[] args)
+    {
+        string methodName = "Update" + args[0];
+        var method = GetType().GetMethod(methodName);
+
+        if (method != null)
+        {
+            object[] parameters = { args[1] };
+            method.Invoke(this, parameters);
+        }
+    }
+
+    public void UpdateLogextension(string logExtension)
+    { 
+        ConfigModel = new ConfigModel();
+        logExtension = "." + logExtension.ToLower();
+        ConfigModel.UpdateConfigFile(logExtension, null);
+    }
+
+    public void UpdateLang(string lang)
+    {
+        ConfigModel = new ConfigModel();
+        ConfigModel.UpdateConfigFile(null, lang.ToLower());
+    }
+}
