fd78ed3af730d5296197b2b93bab6156b3fddd82
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-12-08 16:23:20 +0100

    refactor: rewrite CopyModel.cs before the integration of CryptoSoft

--- EasySave/EasySave/Models/CopyModel.cs
+++ EasySave/EasySave/Models/CopyModel.cs
@@ -5,105 +5,120 @@ namespace EasySave.Models;
 
 public class CopyModel
 {
-    public event Action<string[]> FileCopied;
+    public event Action<string[]>? FileCopied;
 
     // Properties
-    public string SourcePath { get; set; }
-    public string DestPath { get; set; }
-    public BackupType Type { get; set; }
-    public int LeftFilesNumber { get; set; }
-    public long LeftFilesSize { get; set; }
-    public Dictionary<string, List<string>> DirectoryStructure { get; set; }
+    
+    //// Task properties
+    private string SourcePath { get; }
+    private string DestPath { get; }
+    private BackupType Type { get; }
+    // CHANGE CODE TO MAKE THE FOLLOWING PROPERTIES PRIVATE
+    public int LeftFilesNumber { get; private set; }
+    public long LeftFilesSize { get; private set; }
+    // END OF CHANGE
+    
+    //// Directory structure
+    private Dictionary<string, List<string>> DirectoryStructure { get; }
 
     // Constructors
     public CopyModel(string sourcePath, string destPath, BackupType type)
     {
+        // Task properties
         SourcePath = sourcePath;
         DestPath = destPath;
-        DirectoryStructure = new Dictionary<string, List<string>>();
         Type = type;
         LeftFilesNumber = 0;
         LeftFilesSize = 0;
+        
+        // Directory structure
+        DirectoryStructure = new Dictionary<string, List<string>>();
+        
         CheckFiles();
     }
 
     // Methods
-    public void CheckFiles()
+    private void CheckFiles()
     {
-        if (!Directory.Exists(SourcePath))
-        {
+        if (!Directory.Exists(SourcePath) || !Directory.Exists(DestPath))
             throw new DirectoryNotFoundException();
-        }
-
-        if (!Directory.Exists(DestPath))
-        {
-            throw new DirectoryNotFoundException();
-        }
 
         BuildDirectoryStructure(SourcePath, SourcePath);
+    }
+    
+    private bool IsFileModified(string filePath, string relativePath)
+    {
+        var fileName = Path.GetFileName(filePath);
+        var destFilePath = Path.Combine(DestPath, relativePath, fileName);
+
+        return !File.Exists(destFilePath) || File.GetLastWriteTime(filePath) > File.GetLastWriteTime(destFilePath);
     }
 
     private void BuildDirectoryStructure(string currentDirectory, string rootDirectory)
     {
-        string relativePath = currentDirectory.Substring(rootDirectory.Length).TrimStart('\\');
-
+        var relativePath = currentDirectory.Substring(rootDirectory.Length).TrimStart('\\');
+        var currentDirectoryComplete = Directory.GetFiles(currentDirectory);
+        
+        // If the current directories structure doesn't exist in DirectoryStructure, create it
         if (!DirectoryStructure.ContainsKey(relativePath))
-        {
             DirectoryStructure[relativePath] = new List<string>();
-        }
-
-        foreach (string file in Directory.GetFiles(currentDirectory))
+        
+        // In differential save, keep in memory the last used file
+        foreach (var file in currentDirectoryComplete)
         {
-            if (Type == BackupType.Complete || IsFileModified(file, relativePath))
-            {
-                DirectoryStructure[relativePath].Add(Path.GetFileName(file));
-                LeftFilesNumber++;
-                LeftFilesSize += new FileInfo(file).Length;
-            }
+            if (Type != BackupType.Complete && !IsFileModified(file, relativePath)) continue;
+
+            var fileName = Path.GetFileName(file);
+            DirectoryStructure[relativePath].Add(fileName);
+            
+            LeftFilesNumber++;
+            LeftFilesSize += new FileInfo(file).Length;
         }
 
-        foreach (string subdirectory in Directory.GetDirectories(currentDirectory))
+        foreach (var subdirectory in currentDirectoryComplete)
         {
             BuildDirectoryStructure(subdirectory, rootDirectory);
         }
     }
 
-    private bool IsFileModified(string filePath, string relativePath)
-    {
-        string fileName = Path.GetFileName(filePath);
-        string destFilePath = Path.Combine(DestPath, relativePath, fileName);
-
-        return !File.Exists(destFilePath) || File.GetLastWriteTime(filePath) > File.GetLastWriteTime(destFilePath);
-    }
-
     public void CopyFiles()
     {
-        foreach (var directoryEntry in DirectoryStructure)
+        foreach (var directory in DirectoryStructure)
         {
-            string sourceDirectory = Path.Combine(SourcePath, directoryEntry.Key);
-            string destDirectory = Path.Combine(DestPath, directoryEntry.Key);
+            var sourceDirectory = Path.Combine(SourcePath, directory.Key);
+            var destDirectory = Path.Combine(DestPath, directory.Key);
 
             if (!Directory.Exists(destDirectory))
-            {
                 Directory.CreateDirectory(destDirectory);
-            }
 
-            foreach (string fileName in directoryEntry.Value)
+            foreach (var file in directory.Value)
             {
-                string sourceFilePath = Path.Combine(sourceDirectory, fileName);
-                string destFilePath = Path.Combine(destDirectory, fileName);
+                var sourceFilePath = Path.Combine(sourceDirectory, file);
+                var destFilePath = Path.Combine(destDirectory, file);
                 
+                // Use a stop watch to get the time it takes to copy a file
                 var stopwatch = new Stopwatch();
+                
                 stopwatch.Start();
+                
                 File.Copy(sourceFilePath, destFilePath, true);
+                
                 stopwatch.Stop();
                 
                 var copyTime = stopwatch.ElapsedMilliseconds;
                 
                 LeftFilesNumber--;
                 LeftFilesSize -= new FileInfo(sourceFilePath).Length;
-
-                string[] fileInfo = { sourceFilePath, destFilePath, new FileInfo(sourceFilePath).Length.ToString(), copyTime.ToString() };
+                
+                // Invoke file info data to write it in the log file
+                string[] fileInfo =
+                {
+                    sourceFilePath, 
+                    destFilePath, 
+                    new FileInfo(sourceFilePath).Length.ToString(), 
+                    copyTime.ToString()
+                };
+                
                 FileCopied?.Invoke(fileInfo);
             }
         }
