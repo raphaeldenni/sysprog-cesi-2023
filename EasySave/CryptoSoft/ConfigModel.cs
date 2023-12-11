fe1ed542997dd40bdcae33658fe5c0ff17a33221
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-12-11 16:24:36 +0100

    refactor: delete useless methods in CryptoSoft ConfigModel.cs

--- EasySave/CryptoSoft/ConfigModel.cs
+++ EasySave/CryptoSoft/ConfigModel.cs
@@ -16,47 +16,13 @@ public class ConfigModel
     public ConfigEntity? Config { get; private set; }
 
     public ConfigModel()
-    {
-        if (!File.Exists(ConfigFileName))
-        {
-            CreateConfigFile();
-        }
-
+    { 
         PullConfigFile();
-    }
-
-    private void CreateConfigFile()
-    {
-        var defaultConfig = new ConfigEntity
-        {
-            LogExtension = LogType.Json,
-            Language = LangType.En,
-            Key = "0123456789ABCDEF"
-        };
-
-        var defaultConfigJson = JsonSerializer.Serialize(defaultConfig);
-        File.WriteAllText(ConfigFileName, defaultConfigJson);
     }
 
     private void PullConfigFile()
     {
         var configJson = File.ReadAllText(ConfigFileName);
         Config = JsonSerializer.Deserialize<ConfigEntity>(configJson);
-    }
-
-    public void UpdateConfigFile(LogType? logExtension, LangType? lang)
-    {
-        if (logExtension != null)
-        {
-            Config!.LogExtension = (LogType)logExtension;
-        }
-
-        if (lang != null)
-        {
-            Config!.Language = (LangType)lang;
-        }
-
-        var updatedConfigJson = JsonSerializer.Serialize(Config);
-        File.WriteAllText(ConfigFileName, updatedConfigJson);
     }
 }
