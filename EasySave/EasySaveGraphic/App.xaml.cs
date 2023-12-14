7c5c80c73991b66c83fc626d3dc4abc7c2515bae
Bourrel <antonin.bourrel@viacesi.fr>
2023-12-14 15:51:18 +0100

    feat: mono instance

--- EasySave/EasySaveGraphic/App.xaml.cs
+++ EasySave/EasySaveGraphic/App.xaml.cs
@@ -1,27 +1,38 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Threading;
 using System.Windows;
 using EasySave.Models;
 
 namespace EasySaveGraphic
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private const string MutexName = "Global\\EasySaveApplicationMutex";
+        private static Mutex mutex;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
+            // Use a Mutex to ensure mono-instance
+            mutex = new Mutex(true, MutexName, out bool createdNew);
 
-            // Load the language setting from the config.json file
+            if (!createdNew)
+            {
+                // Une autre instance de l'application est déjà en cours d'exécution
+                MessageBox.Show("Une autre instance de l'application est déjà en cours d'exécution.", "EasySave", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                Shutdown();
+                return;
+            }
+
+            // Charger les paramètres de langue depuis le fichier config.json
             string language = LoadLang();
 
-            // Set the CultureInfo of the current thread to the loaded language
+            // Définir la culture de l'interface utilisateur (UI) en fonction de la langue chargée
             System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
 
-            // Set the Culture property of the Resources class
+            // Définir la culture des ressources de l'application
             EasySaveGraphic.Lang.Resources.Culture = System.Threading.Thread.CurrentThread.CurrentUICulture;
+
+            base.OnStartup(e);
         }
 
         private string LoadLang()
@@ -29,6 +40,14 @@ namespace EasySaveGraphic
             var model = new ConfigModel();
             return model.Config.Language.ToString();
         }
-    }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Libérer la ressource Mutex lors de la fermeture de l'application
+            mutex.ReleaseMutex();
+            mutex.Dispose();
+
+            base.OnExit(e);
+        }
+    }
 }
