fa0eccfb9df24cd326fe8812c664a12050d01e59
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-12-15 10:13:26 +0100

    chore: initial code commit for EasySaveNet

---
+++ EasySave/EasySaveNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Text;
+
+namespace EasySaveNet;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
